0544e2ca5bee834006097b927b338a3529ed0dd6
Ahmed Wael Salah <88168055+AhmedWaeI@users.noreply.github.com>
2023-12-17 12:43:31 +0200

    Update priority_resolver(incomplete).v

--- priority_resolver(incomplete).v
+++ priority_resolver(incomplete).v
@@ -1,96 +1,54 @@
-module Priority_Resolver (
- 
-  input [7:0] interrupt_mask,
-
-  
-  // Inputs  
-  input [7:0] interrupt_request_register,
-  input [7:0] in_service_register,
-  input mode,
-  // Outputs
-  output [7:0] interrupt
+module PriorityResolver (
+    input wire [7:0] IRR,  // Interrupt Request Register
+    input wire [7:0] IMR,  // Interrupt Mask Register
+    input wire [2:0] OCW,  // Operation Control Words OCW2 (L2, L1, L0) and OCW3 (ESMM, SMM)
+    output reg [7:0] ISR   // Interrupt Service Register
 );
 
+    reg [7:0] masked_IRR;      // Masked Interrupt Request Register
+    reg [7:0] masked_priority; // Masked priority register
+    reg highest_priority;
 
-  wire [7:0] masked_interrupt_request;
-  parameter anothermode_irr=interrupt_request_register;
+    // Register to store the last serviced priority for rotation
+    reg [2:0] last_serviced_priority = 3'b000;
 
-  assign masked_interrupt_request = interrupt_request_register & ~interrupt_mask;
-  if(mode==0)
-    begin
-      
-    always(interrupt_request_register)
-      begin
-        if(masked_interrupt_request[0]==1'b)
-          interrupt=8'b1;
-        if(masked_interrupt_request[1]==1'b)
-          interrupt=8'b10;
-        if(masked_interrupt_request[2]==1'b)
-          interrupt=8'b100;
-        if(masked_interrupt_request[3]==1'b)
-          interrupt=8'b1000;
-        if(masked_interrupt_request[4]==1'b)
-          interrupt=8'b10000;
-        if(masked_interrupt_request[5]==1'b)
-          interrupt=8'b100000;
-        if(masked_interrupt_request[6]==1'b)
-          interrupt=8'b1000000;
-        if(masked_interrupt_request[7]==1'b)
-          interrupt=8'b10000000;
-      end
+    always @* begin
+        // Apply masking based on OCW1 and IMR
+        masked_IRR = IRR & ~IMR & ~{3'b0, OCW[2:0]};
+
+        // Logic to determine the highest priority bit in the masked IRR
+        masked_priority = masked_IRR & (masked_IRR ^ (masked_IRR - 1));
+        highest_priority = $ff & ((masked_priority ^ (masked_priority - 1)) >> 1);
+
+        // Fully Nested Mode
+        if (OCW[2:0] == 3'b000) begin
+            // Check if highest priority interrupt is acknowledged
+            if (highest_priority != 8'b0000_0000) begin
+                // Strobing the highest priority into the ISR during INTA pulse or as required
+                ISR[highest_priority] <= 1;
+
+                // Mask lower priority interrupts until EOI
+                ISR <= ISR & ((1 << highest_priority) - 1);
+
+                // Check if EOI is issued
+                if (OCW[3] == 1'b1) begin
+                    // Handle End of Interrupt (EOI) logic
+                    // Clear ISR bit after EOI command
+                end
+            }
+        }
+
+        // Rotation Mode
+        else if (OCW[2:0] != 3'b000) begin
+            // Check if any interrupt is pending
+            if (highest_priority != 8'b0000_0000) begin
+                // Rotate priorities
+                if (highest_priority != last_serviced_priority) begin
+                    last_serviced_priority <= highest_priority;
+                    ISR <= {ISR[highest_priority], ISR[7:1]};
+                end
+            end
+        }
     end
-  else
-    begin
-      always(interrupt_request_register)
-        begin
-          if(masked_interrupt_request[0]==1'b)
-            begin
-              anothermode_irr={anothermode_irr[0], anothermode_irr[7:1]};
-              masked_interrupt_request=anothermode_irr & ~interrupt_mask;
-              interrupt=8'b1;
-            end
-          if(masked_interrupt_request[1]==1'b)
-            begin
-              anothermode_irr={anothermode_irr[1:0], anothermode_irr[7:2]};
-              masked_interrupt_request=anothermode_irr & ~interrupt_mask;
-              interrupt=8'b10;
-            end
-          if(masked_interrupt_request[2]==1'b)
-            begin
-              anothermode_irr={anothermode_irr[2:0], anothermode_irr[7:3]};
-              masked_interrupt_request=anothermode_irr & ~interrupt_mask;
-              interrupt=8'b100;
-            end
-          if(masked_interrupt_request[3]==1'b)
-            begin
-              anothermode_irr={anothermode_irr[3:0], anothermode_irr[7:4]};
-              masked_interrupt_request=anothermode_irr & ~interrupt_mask;
-              interrupt=8'b1000;
-            end
-          if(masked_interrupt_request[4]==1'b)
-            begin
-              anothermode_irr={anothermode_irr[4:0], anothermode_irr[7:5]};
-              masked_interrupt_request=anothermode_irr & ~interrupt_mask;
-              interrupt=8'b10000;
-            end
-          if(masked_interrupt_request[5]==1'b)
-            begin
-              anothermode_irr={anothermode_irr[5:0], anothermode_irr[7:6]};
-              masked_interrupt_request=anothermode_irr & ~interrupt_mask;
-              interrupt=8'b100000;
-            end
-          if(masked_interrupt_request[6]==1'b)
-            begin
-              anothermode_irr={anothermode_irr[6:0], anothermode_irr[7]};
-              masked_interrupt_request=anothermode_irr & ~interrupt_mask;
-              interrupt=8'10000000;
-            end
-          if(masked_interrupt_request[7]==1'b)
-            begin
-              anothermode_irr={anothermode_irr[0], anothermode_irr[7:1]};
-              masked_interrupt_request=anothermode_irr & ~interrupt_mask;
-              interrupt=8'b10000000;
-            end
-        end
-    end
+
 endmodule
