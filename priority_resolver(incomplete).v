62d62e87bca6fd8e93d89c2b9db1e2b2a5d16295
Ahmed Wael Salah <88168055+AhmedWaeI@users.noreply.github.com>
2023-12-21 14:49:23 +0200

    Update priority_resolver(incomplete).v

--- priority_resolver(incomplete).v
+++ priority_resolver(incomplete).v
@@ -1,75 +1,111 @@
-module PriorityResolver (
-    input wire clk,
-    input wire reset,
-    input wire [7:0] irr,
-    input wire [7:0] imr,
-    input wire [3:0] ocw,
-    input wire inta,
-    input wire eoi,
-    output reg [7:0] isr,
-    output wire [2:0] highest_priority
+module rotate_right (
+    input [7:0] source,
+    input [2:0] rotate,
+    output reg [7:0] rotate_right_output
 );
 
-    reg [7:0] masked_irr;
-    reg [7:0] masked_priority;
-    reg [2:0] last_serviced_priority;
-    reg rotation_enabled;
-    reg eoi_received;
-    reg [2:0] automatic_priority;
-
-    reg [2:0] priority_wire;
-	
     always @* begin
-        case(masked_priority)
-            8'b00000001: priority_wire = 3'b000;
-            8'b00000010: priority_wire = 3'b001;
-            8'b00000100: priority_wire = 3'b010;
-            8'b00001000: priority_wire = 3'b011;
-            8'b00010000: priority_wire = 3'b100;
-            8'b00100000: priority_wire = 3'b101;
-            8'b01000000: priority_wire = 3'b110;
-            8'b10000000: priority_wire = 3'b111;
-            default: priority_wire = 3'b111; // Set a default value
+        case(rotate)
+            3'b000: rotate_right_output = { source[0], source[7:1] };
+            3'b001: rotate_right_output = { source[1:0], source[7:2] };
+            3'b010: rotate_right_output = { source[2:0], source[7:3] };
+            3'b011: rotate_right_output = { source[3:0], source[7:4] };
+            3'b100: rotate_right_output = { source[4:0], source[7:5] };
+            3'b101: rotate_right_output = { source[5:0], source[7:6] };
+            3'b110: rotate_right_output = { source[6:0], source[7] };
+            default: rotate_right_output = source;
         endcase
     end
 
-    assign highest_priority = priority_wire;
+endmodule
+
+module resolv_priority(
+    input [7:0] request,
+    output reg [7:0] resolv_priority
+);
 
     always @* begin
-        masked_irr = irr & ~imr & ~{3'b0, ocw[2:0]};
-        masked_priority = masked_irr;
-        
-        rotation_enabled = ocw[2];
-        eoi_received = eoi;
-
-        if (ocw == 3'b000) begin
-            if (masked_priority != 8'b0 && inta)
-                last_serviced_priority = highest_priority;
-            
-            if (eoi && masked_priority != 8'b0)
-                isr[last_serviced_priority] <= 1'b0;
-        end else begin
-            if (rotation_enabled && eoi_received && ocw == 3'b001 && inta) begin
-                automatic_priority <= (last_serviced_priority + 1) % 8;
-                if (masked_priority[automatic_priority] && automatic_priority != last_serviced_priority) begin
-                    last_serviced_priority <= automatic_priority;
-                    isr[last_serviced_priority] <= 1'b1;
-                end
-            end
-        end
+        if (request[0] == 1'b1) resolv_priority = 8'b00000001;
+        else if (request[1] == 1'b1) resolv_priority = 8'b00000010;
+        else if (request[2] == 1'b1) resolv_priority = 8'b00000100;
+        else if (request[3] == 1'b1) resolv_priority = 8'b00001000;
+        else if (request[4] == 1'b1) resolv_priority = 8'b00010000;
+        else if (request[5] == 1'b1) resolv_priority = 8'b00100000;
+        else if (request[6] == 1'b1) resolv_priority = 8'b01000000;
+        else if (request[7] == 1'b1) resolv_priority = 8'b10000000;
+        else resolv_priority = 8'b00000000;
     end
 
-    always @(posedge clk or posedge reset) begin
-        if (!reset) begin
-            last_serviced_priority <= 0;
-            isr <= 8'b0;
-        end else begin
-            if (ocw == 3'b000 && masked_priority != 8'b0)
-              begin
-                isr <= 8'b0;
-                isr[highest_priority] <= 1'b1;
-              end
-        end
-    end
+endmodule
+
+
+module PriorityResolver (
+  // Inputs from control logic
+  input   [2:0]   priority_rotate,
+  input   [7:0]   interrupt_mask,
+  input   [7:0]   interrupt_special_mask,
+  input           special_fully_nest_config,
+  input   [7:0]   highest_level_in_service,
+
+  // Inputs
+  input   [7:0]   interrupt_request_register,
+  input   [7:0]   in_service_register,
+
+  // Outputs
+  output  [7:0]   interrupt
+);
+  // Masked flags
+  reg   [7:0]   masked_interrupt_request;
+  reg   [7:0]   masked_in_service;
+
+  // Resolve priority
+  reg   [7:0]   rotated_request;
+  reg   [7:0]   rotated_highest_level_in_service;
+  reg   [7:0]   rotated_in_service;
+  reg   [7:0]   priority_mask;
+  reg   [7:0]   rotated_interrupt;
+  reg   [7:0]   resolv_priority;
+  rotate_left_right m
+
+  // Rotate and resolve priority logic
+  always@(*) begin
+    masked_interrupt_request = interrupt_request_register & ~interrupt_mask;
+    masked_in_service = in_service_register & ~interrupt_special_mask;
+    rotate_right m(masked_interrupt_request,priority_rotate,rotated_request)
+    rotate_right n(highest_level_in_service,priority_rotate,rotated_highest_level_in_service)
+    
+    if (special_fully_nest_config == 1'b1)
+      rotated_in_service = {masked_in_service[7-priority_rotate:0],masked_in_service[7:7-priority_rotate]} & ~rotated_highest_level_in_service | {rotated_highest_level_in_service[6:0], 1'b0};
+    else
+      rotated_in_service = {masked_in_service[7-priority_rotate:0],masked_in_service[7:7-priority_rotate]};
+  end
+
+  always@(*) begin
+    if      (rotated_in_service[0] == 1'b1) priority_mask = 8'b00000000;
+    else if (rotated_in_service[1] == 1'b1) priority_mask = 8'b00000001;
+    else if (rotated_in_service[2] == 1'b1) priority_mask = 8'b00000011;
+    else if (rotated_in_service[3] == 1'b1) priority_mask = 8'b00000111;
+    else if (rotated_in_service[4] == 1'b1) priority_mask = 8'b00001111;
+    else if (rotated_in_service[5] == 1'b1) priority_mask = 8'b00011111;
+    else if (rotated_in_service[6] == 1'b1) priority_mask = 8'b00111111;
+    else if (rotated_in_service[7] == 1'b1) priority_mask = 8'b01111111;
+    else                                   priority_mask = 8'b11111111;
+  end
+
+  always@(*) begin
+    if      (rotated_request[0] == 1'b1) resolv_priority = 8'b00000001;
+    else if (rotated_request[1] == 1'b1) resolv_priority = 8'b00000010;
+    else if (rotated_request[2] == 1'b1) resolv_priority = 8'b00000100;
+    else if (rotated_request[3] == 1'b1) resolv_priority = 8'b00001000;
+    else if (rotated_request[4] == 1'b1) resolv_priority = 8'b00010000;
+    else if (rotated_request[5] == 1'b1) resolv_priority = 8'b00100000;
+    else if (rotated_request[6] == 1'b1) resolv_priority = 8'b01000000;
+    else if (rotated_request[7] == 1'b1) resolv_priority = 8'b10000000;
+    else                                resolv_priority = 8'b00000000;
+  end
+
+  
+  assign rotated_interrupt =  resolv_priority & priority_mask;
+  assign interrupt = {rotated_interrupt[priority_rotate:7],rotated_interrupt[7:priority_rotate+1]}; 
 
 endmodule
