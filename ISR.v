6ee2c26fe3beaaac99f501da3cab62059149c4b4
Maria ashraf <mariaashraf202@gmail.com>
2023-12-21 19:43:22 +0200

    third version

--- ISR.v
+++ ISR.v
@@ -20,36 +20,75 @@ module ISR (
         in_service_register <= next_in_service_register;    //f the reset signal is not asserted, it sets in_service_register to the value of next_in_service_register
    
     reg [7:0] next_highest_level_in_service;
+    reg [7:0]  rotated;
+    reg [7:0]  resolve_priority;
 
-	function [7:0] resolve_priority;                     //this function determines the priority level of the set bits in the request vector, 
-		input [7:0] request;                             //with higher priority assigned to lower bit positions. The output is an 8-bit binary value representing the priority
-		if (request[0] == 1'b1)
+	always@(*) begin      // rotate right 
+      case(priority_rotate)
+            3'b000: rotated = { next_highest_level_in_service[0], next_highest_level_in_service[7:1] };
+            3'b001: rotated = { next_highest_level_in_service[1:0], next_highest_level_in_service[7:2] };
+            3'b010: rotated = { next_highest_level_in_service[2:0], next_highest_level_in_service[7:3] };
+            3'b011: rotated = { next_highest_level_in_service[3:0], next_highest_level_in_service[7:4] };
+            3'b100: rotated = { next_highest_level_in_service[4:0], next_highest_level_in_service[7:5] };
+            3'b101: rotated = { next_highest_level_in_service[5:0], next_highest_level_in_service[7:6] };
+            3'b110: rotated = { next_highest_level_in_service[6:0], next_highest_level_in_service[7] };
+            default: rotated = next_highest_level_in_service;
+      endcase
+   end
+    
+    
+    always@(*) begin
+        if(next_highest_level_in_service[0] == 1'b1)
 			resolve_priority = 8'b00000001;
-		else if (request[1] == 1'b1)
+		else if (next_highest_level_in_service[1] == 1'b1)
 			resolve_priority = 8'b00000010;
-		else if (request[2] == 1'b1)
+		else if (next_highest_level_in_service[2] == 1'b1)
 			resolve_priority = 8'b00000100;
-		else if (request[3] == 1'b1)
+		else if (next_highest_level_in_service[3] == 1'b1)
 			resolve_priority = 8'b00001000;
-		else if (request[4] == 1'b1)
+		else if (next_highest_level_in_service[4] == 1'b1)
 			resolve_priority = 8'b00010000;
-		else if (request[5] == 1'b1)
+		else if (next_highest_level_in_service[5] == 1'b1)
 			resolve_priority = 8'b00100000;
-		else if (request[6] == 1'b1)
+		else if (next_highest_level_in_service[6] == 1'b1)
 			resolve_priority = 8'b01000000;
-		else if (request[7] == 1'b1)
+		else if (next_highest_level_in_service[7] == 1'b1)
 			resolve_priority = 8'b10000000;
 		else
 			resolve_priority = 8'b00000000;
-	endfunction    
-    
+        
+    end
+
+
+
+
+
+
+
+always@(*)  // rotate left 
+    begin
+      case(priority_rotate)
+            3'b000: rotated = { next_highest_level_in_service[6:0], next_highest_level_in_service[7] };
+            3'b001: rotated = { next_highest_level_in_service[5:0], next_highest_level_in_service[7:6] };
+            3'b010: rotated = { next_highest_level_in_service[4:0], next_highest_level_in_service[7:5] };
+            3'b011: rotated = { next_highest_level_in_service[3:0], next_highest_level_in_service[7:4] };
+            3'b100: rotated = { next_highest_level_in_service[2:0], next_highest_level_in_service[7:3] };
+            3'b101: rotated = { next_highest_level_in_service[1:0], next_highest_level_in_service[7:2] };
+            3'b110: rotated = { next_highest_level_in_service[0], next_highest_level_in_service[7:1] };
+            3'b111: rotated = next_highest_level_in_service;
+            default: rotated = next_highest_level_in_service;
+        endcase
+    end 
+
+
+
 
 
 	always @(*) begin
 		next_highest_level_in_service = next_in_service_register & ~interrupt_special_mask;
-		next_highest_level_in_service = KF8259_Common_Package_rotate_right(next_highest_level_in_service, priority_rotate);
-		next_highest_level_in_service = KF8259_Common_Package_resolv_priority(next_highest_level_in_service);
-		next_highest_level_in_service = KF8259_Common_Package_rotate_left(next_highest_level_in_service, priority_rotate);
+		next_highest_level_in_service = rotated;
+		next_highest_level_in_service = resolve_priority;
+		next_highest_level_in_service = rotated;
 	end
 
 
