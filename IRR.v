8ca75c2a63308ddec76cb54bceb6f8216355b2fc
MarlineMansour <131539534+MarlineMansour@users.noreply.github.com>
2023-12-24 20:42:11 +0200

    Update IRR.v

--- IRR.v
+++ IRR.v
@@ -1,7 +1,7 @@
 module Interrupt_Request(
-    input wire Level_OR_Edge_trigger,
-    input wire [7:0] Interrupt_Request_Pins,
-    output reg [7:0] Interrupt_Request_Reg
+    input wire Level_OR_Edge_trigger, //control input
+    input wire [7:0] Interrupt_Request_Pins, //in/out devices 
+    output reg [7:0] Interrupt_Request_Reg   
 );
 
     reg [7:0] last_Int_Req_Pin_value;
@@ -10,7 +10,7 @@ module Interrupt_Request(
     generate
         for ( ir_bit_no = 0; ir_bit_no <= 7; ir_bit_no = ir_bit_no + 1) begin
            always @(Interrupt_Request_Pins) begin
-               if(Level_OR_Edge_trigger == 1'b0) begin
+               if(Level_OR_Edge_trigger == 1'b0) begin   //edge trigger mode is on
                     if ((~last_Int_Req_Pin_value[ir_bit_no] )& Interrupt_Request_Pins[ir_bit_no]) begin
                         Interrupt_Request_Reg[ir_bit_no] <= Interrupt_Request_Pins[ir_bit_no];
                     end else begin
@@ -19,7 +19,7 @@ module Interrupt_Request(
                     end
                 end
 
-               if(Level_OR_Edge_trigger == 1'b1) begin
+               if(Level_OR_Edge_trigger == 1'b1) begin  // level trigger mode is on
                     if (Interrupt_Request_Pins[ir_bit_no] == 1'b1 || Interrupt_Request_Pins[ir_bit_no] == 1'b0) begin
                         Interrupt_Request_Reg[ir_bit_no] <= Interrupt_Request_Pins[ir_bit_no];
                     end else begin
