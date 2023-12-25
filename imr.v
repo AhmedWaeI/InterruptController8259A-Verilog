ad60bb31f3440bd5ad83340811a7a16de8d48976
OmarMaksoud <132158344+OmarMaksoud@users.noreply.github.com>
2023-12-25 15:11:41 +0200

    Update imr.v

--- imr.v
+++ imr.v
@@ -1,15 +1,15 @@
 module IMRModule (
-  input wire [7:0] mask_vector,
-  input wire [7:0] active_interrupts,
-  output wire [7:0] imr_output
+  input wire [7:0] mask_vector,//OCW1 data.
+  output wire [7:0] IMR_output
 );
 
   reg [7:0] imr_register;
 
-  always @* begin
-    imr_register = mask_vector & ~active_interrupts;
+  always @* 
+  begin
+    imr_register = mask_vector
   end
 
-  assign imr_output = imr_register;
+  assign IMR_output = imr_register;
 
 endmodule
