4841f91428b4ee78c3c702c2f0fab681c703fda0
MarlineMansour <131539534+MarlineMansour@users.noreply.github.com>
2024-01-02 21:16:23 +0200

    Update IRR testbench.v

--- IRR testbench.v
+++ IRR testbench.v
@@ -1,7 +1,7 @@
 module Interrupt_Request_tb;
 
   reg Level_OR_Edge_trigger;
-  reg [7:0] Interrupt_Request_Pins;
+  reg [7:0] Interrupt_Request_Pins; 
   wire [7:0] Interrupt_Request_Reg;
 
   // Instantiate the Interrupt_Request module
@@ -17,20 +17,20 @@ module Interrupt_Request_tb;
     $dumpvars;
     // Test case 1: Edge-triggered, 
     Level_OR_Edge_trigger = 0;
-    Interrupt_Request_Pins = 8'b10000001;
+    Interrupt_Request_Pins = 8'b10000001;  //IR0 & IR& are rised 
     #10;
     $display("Test Case 1: Interrupt_Request_Reg = %b", Interrupt_Request_Reg);
     #10  
     // Test case 2: Level-triggered, 
     Level_OR_Edge_trigger = 1;
-    Interrupt_Request_Pins = 8'b11001111;
+    Interrupt_Request_Pins = 8'b11001111; //IR0 ,IR1 ,IR2 ,IR3 , IR6 , IR7 are rised 
     #10;
     $display("Test Case 2: Interrupt_Request_Reg = %b", Interrupt_Request_Reg);
 
     
 // Test case 3: Level-triggered,
     Level_OR_Edge_trigger = 1;
-    Interrupt_Request_Pins = 8'b11010011;
+    Interrupt_Request_Pins = 8'b11010011; //IR0 ,IR1 ,IR4 ,IR6 ,IR7 are rised 
     #10;
     $display("Test Case 3: Interrupt_Request_Reg = %b", Interrupt_Request_Reg);
 
